1e986687be5f596ff5966263e604267eb723f316
Yehuda Nevo <45956551+YehudaNevo@users.noreply.github.com>
2023-05-09 16:09:11 +0300

    implement the pptx class and add test for it

---
+++ pptx_parser.py
@@ -0,0 +1,53 @@
+import os
+from typing import Dict, Union
+from pptx import Presentation
+from pptx.shapes.base import BaseShape
+
+
+class PptxParser:
+    def __init__(self, pptx_file_path: str):
+        if not os.path.exists(pptx_file_path):
+            raise FileNotFoundError(f"File not found: {pptx_file_path}")
+        self.pptx_file_path = pptx_file_path
+
+    def _get_text_from_shape(self, shape: BaseShape) -> str:
+        text = ""
+        if shape.has_text_frame:
+            paragraphs = [p.text.strip() for p in shape.text_frame.paragraphs]
+            text = " ".join(paragraphs)
+        return text
+
+    def parse_presentation(self) -> Dict[int, str]:
+        ppt = Presentation(self.pptx_file_path)
+        slides_dict = {}
+
+        for slide_num, slide in enumerate(ppt.slides, start=1):
+            slide_text = []
+            for shape in slide.shapes:
+                text = self._get_text_from_shape(shape)
+                if text:
+                    slide_text.append(text)
+            slides_dict[slide_num] = " ".join(slide_text)
+
+        return slides_dict
+
+
+
+def main():
+    # Replace with the path to your PowerPoint file
+    pptx_file_path = "./test/files/presentation_for_tst.pptx"
+
+    # Create an instance of PptxParser with the file path
+    pptx_parser = PptxParser(pptx_file_path)
+
+    # Parse the presentation and store the resulting dictionary
+    slides_dict = pptx_parser.parse_presentation()
+
+    # Print the dictionary
+    for slide_num, slide_text in slides_dict.items():
+        print(f"Slide {slide_num}:")
+        print(slide_text)
+        print()
+
+if __name__ == "__main__":
+    main()
---
+++ test/test_pptx_parser.py
@@ -0,0 +1,19 @@
+
+import unittest
+from pptx_parser import PptxParser
+
+class TestPptxParser(unittest.TestCase):
+    def test_parse_presentation(self):
+        pptx_parser = PptxParser("./files/presentation_for_tst.pptx")
+        slides_dict = pptx_parser.parse_presentation()
+
+        # Expected dictionary based on the test PowerPoint file
+        expected_dict = {
+            1: "PE format structure and explanation",
+            2: "IDA -  tool for reverse exe file"  # Keep the extra space here
+        }
+
+        self.assertDictEqual(slides_dict, expected_dict)
+
+if __name__ == '__main__':
+    unittest.main()
